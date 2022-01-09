a28d2c9e58932571385c4c13e73d1b2680589542
SpoodyDev <91509772+SpoodyDev@users.noreply.github.com>
2022-01-09 05:45:38 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.DriverShoot = true -- true if the driver can shoot
+
+Config.WhitelistedVehicles = { -- Cars that you can shoot from
+    ["adder"] = true,
+    ["t20"] = true,
+}
