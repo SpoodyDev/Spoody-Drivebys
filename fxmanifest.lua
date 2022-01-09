4ab4bb14b54dc8a65d3416ff028a649eb58b8780
SpoodyDev <91509772+SpoodyDev@users.noreply.github.com>
2022-01-09 05:45:59 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Spoody'
+description 'Spoody Driveby'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+}
+
+server_script 'server.lua'
